935423a64c9d4638da0ec0594dd9b44fea3f4333
vyvenlee <yvenlee.voninkabel@ynov.com>
2025-07-01 17:11:35 +0200

    data

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def scrape():
         driver.quit()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=True)
